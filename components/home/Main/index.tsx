9724dec6c5bc1c5d17b92d35bb2adb4abf496e9d
wanxiankai <wanxiankai@126.com>
2023-12-31 23:30:42 +0800

    feat: 封装消息列表组件

--- components/home/Main/index.tsx
+++ components/home/Main/index.tsx
@@ -1,5 +1,6 @@
 import ChatInput from "./ChatInput";
 import Menu from "./Menu";
+import MessageList from "./MessageList";
 import Welcome from "./Welcome";
 
 export default function Main() {
@@ -9,7 +10,8 @@ export default function Main() {
                 className="overflow-y-auto w-full h-full bg-white text-gray-900 dark:bg-gray-800 dark:text-gray-100"
             >
                 <Menu />
-                <Welcome />
+                {/* <Welcome /> */}
+                <MessageList/>
                 <ChatInput />
             </main>
         </div>
