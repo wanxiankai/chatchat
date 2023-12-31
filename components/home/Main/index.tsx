81b05f92aef5a39c943a746ca1c894912d0376e4
wanxiankai <wanxiankai@126.com>
2023-12-31 20:15:49 +0800

    feat: 使用欢迎组件

--- components/home/Main/index.tsx
+++ components/home/Main/index.tsx
@@ -1,4 +1,5 @@
 import Menu from "./Menu";
+import Welcome from "./Welcome";
 
 export default function Main() {
     return (
@@ -6,5 +7,6 @@ export default function Main() {
             className="relative flex-1 bg-white text-gray-900 dark:bg-gray-800 dark:text-gray-100"
         >
             <Menu />
+            <Welcome />
         </main>)
 }
