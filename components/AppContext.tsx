4c8e8a9f20ed57eabf08adf7aad51077de20a98a
wanxiankai <wanxiankai@126.com>
2023-12-31 12:06:14 +0800

    test: 测试不使用usemome 会导致的渲染问题

--- components/AppContext.tsx
+++ components/AppContext.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { createContext, Dispatch, ReactNode, SetStateAction, useMemo, useState } from "react"
+import { createContext, Dispatch, ReactNode, SetStateAction, useContext, useMemo, useState } from "react"
 
 type State = {
     displayNavigation: boolean
@@ -13,14 +13,23 @@ type AppContextProps = {
 
 const AppContext = createContext<AppContextProps>(null!)
 
+export function useAppContext() {
+    return useContext(AppContext)
+}
+
 export default function AppContextProvider({ children }: { children: ReactNode }) {
     const [state, setState] = useState({ displayNavigation: true })
+    const [test, setTest] = useState(true)
+    console.log("AppContextProvider")
     const contextValue = useMemo(() => {
         return { state, setState }
     }, [state, setState])
 
     return (
-        <AppContext.Provider value={contextValue}>
+        <AppContext.Provider value={{ state, setState }}>
+            <button onClick={() => {
+                setTest((v) => !v )
+                }}>Test</button>
             {children}
         </AppContext.Provider>
     )
--- components/home/Navigation/index.tsx
+++ components/home/Navigation/index.tsx
@@ -1,8 +1,12 @@
+"use client"
+import { useAppContext } from '@/components/AppContext'
 import MenuBar from './MenuBar'
 
-export default function Navigation(){
+export default function Navigation() {
+    const { state: { displayNavigation } } = useAppContext()
+    console.log('Navigation render')
     return (
-    <nav className="h-full w-[260px] bg-gray-900 text-gray-300 p-2">
-        <MenuBar/>
-    </nav>)
+        <nav className="h-full w-[260px] bg-gray-900 text-gray-300 p-2">
+            <MenuBar />
+        </nav>)
 }
