68d0eb345fe2462e72011f09ece27cfc1ae97cf3
wanxiankai <wanxiankai@126.com>
2023-12-31 23:54:26 +0800

    feat: 使用remarkgfm 库实现识别链接

--- components/common/MarkDown.tsx
+++ components/common/MarkDown.tsx
@@ -1,9 +1,11 @@
 import { memo } from 'react'
 import ReactMarkDown, { Options } from 'react-markdown'
+import remarkGfm from 'remark-gfm'
 
 function MarkDown({ children, className = '', ...props }: Options) {
     return (
         <ReactMarkDown
+            remarkPlugins={[remarkGfm]}
             className={`markdown prose dark:prose-invert ${className}`}
             {...props}
         >
