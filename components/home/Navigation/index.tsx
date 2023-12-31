49c0b264e877bb4311f96cc19502a79c48f31b37
wanxiankai <wanxiankai@126.com>
2023-12-31 13:03:08 +0800

    feat: 导航组件新增底部工具栏

--- components/home/Navigation/index.tsx
+++ components/home/Navigation/index.tsx
@@ -1,13 +1,15 @@
 "use client"
 import { useAppContext } from '@/components/AppContext'
 import MenuBar from './MenuBar'
+import ToolsBar from './ToolBar'
 
 export default function Navigation() {
     const { state: { displayNavigation } } = useAppContext()
     return (
         <nav className={`${
             displayNavigation ? '':'hidden' 
-        } h-full w-[260px] bg-gray-900 text-gray-300 p-2`} >
+        } dark relative h-full w-[260px] bg-gray-900 text-gray-300 p-2`} >
         <MenuBar />
+        <ToolsBar/>
         </nav >)
 }
