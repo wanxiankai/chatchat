c4ddb204bb56e1f25293e9e28f0f32c5914cd94d
wanxiankai <wanxiankai@126.com>
2023-12-31 13:02:41 +0800

    feat: 将themMode 控制转移到page 中

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
+"use client"
+import { useAppContext } from '@/components/AppContext'
 import Main from '@/components/home/Main'
 import Navigation from '@/components/home/Navigation'
 
 export default function Home() {
+  const { state: { themeMode } } = useAppContext();
   return (
-    <div className='h-full flex'>
-      <Navigation/>
-      <Main/>
+    <div className={`${themeMode} h-full flex`}>
+      <Navigation />
+      <Main />
     </div>
   )
 }
