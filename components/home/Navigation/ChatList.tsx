b0dc473b8acf7c22c5a917bf2a34d259748e0234
wanxiankai <wanxiankai@126.com>
2024-01-02 00:18:20 +0800

    feat: 实现对话列表上拉加载更多

--- components/home/Navigation/ChatList.tsx
+++ components/home/Navigation/ChatList.tsx
@@ -10,6 +10,9 @@ import { ActionType } from "@/reducers/AppReducer"
 export default function ChatList() {
     const [chatList, setChatList] = useState<Chat[]>([])
     const pageRef = useRef(1);
+    const loadMoreRef = useRef(null);
+    const hasMoreRef = useRef(false);
+    const loadingRef = useRef(false)
     const { state: { selectedChat }, dispatch } = useAppContext()
 
     const groupList = useMemo(() => {
@@ -19,20 +22,45 @@ export default function ChatList() {
 
     async function getData() {
         console.log('getChatList')
-        const response = await fetch(`/api/list?page=${pageRef.current}`, {
+        if (loadingRef.current) return;
+        loadingRef.current = true
+        const response = await fetch(`/api/chat/list?page=${pageRef.current}`, {
             method: "GET"
         })
         if (!response.ok) {
+            loadingRef.current = false
             console.log('对话列表请求失败', response.statusText)
             return
         }
         const { data } = await response.json()
+        hasMoreRef.current = data.hasMore;
         if (pageRef.current === 1) {
             setChatList(data.list)
         } else {
-            setChatList([...chatList, data.list])
+            setChatList((list) => list.concat(data.list))
         }
+        pageRef.current++;
+        loadingRef.current = false
     }
+
+    useEffect(() => {
+        let observer: IntersectionObserver | null = null;
+        let div = loadMoreRef.current;
+        if (div) {
+            observer = new IntersectionObserver((entries) => {
+                if (entries[0].isIntersecting && hasMoreRef.current) {
+                    console.log('visible')
+                    getData();
+                }
+            })
+            observer.observe(div)
+        }
+        return () => {
+            if (observer && div) {
+                observer.unobserve(div)
+            }
+        }
+    }, [])
 
     useEffect(() => {
         console.log('effect')
@@ -61,19 +89,20 @@ export default function ChatList() {
                             {
                                 list.map((item) => {
                                     const selected = selectedChat?.id === item.id;
-                                    return <ChatItem 
-                                                key={item.id} 
-                                                item={item} 
-                                                selected={selected} 
-                                                onSelected={(chat) => {
-                                                    dispatch({type: ActionType.UPDATE, fiel:'selectedChat', value: chat})
-                                                }} />
+                                    return <ChatItem
+                                        key={item.id}
+                                        item={item}
+                                        selected={selected}
+                                        onSelected={(chat) => {
+                                            dispatch({ type: ActionType.UPDATE, fiel: 'selectedChat', value: chat })
+                                        }} />
                                 })
                             }
                         </ul>
                     </div>
                 )
             })}
+            <div ref={loadMoreRef}>&nbsp;</div>
         </div>
     )
 }
