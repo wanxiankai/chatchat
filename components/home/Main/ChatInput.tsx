22b014061af9a97d843ec37bcd35f3df9d122d8e
wanxiankai <wanxiankai@126.com>
2024-01-01 17:18:56 +0800

    feat: 优化停止生成与重新生成按钮的显示逻辑

--- components/home/Main/ChatInput.tsx
+++ components/home/Main/ChatInput.tsx
@@ -1,6 +1,6 @@
 import Button from "@/components/common/Button";
 import { MdRefresh } from 'react-icons/md'
-import { PiLightningFill } from 'react-icons/pi'
+import { PiLightningFill, PiStopBold } from 'react-icons/pi'
 import { FiSend } from 'react-icons/fi'
 import TextareaAutoSize from 'react-textarea-autosize'
 import { useState } from "react";
@@ -68,13 +68,24 @@ export default function ChatInput() {
     return (
         <div className="absolute bottom-0 inset-x-0 bg-gradient-to-b from-[rgba(255,255,255,0)] from-[13.94%] to-[#fff] to-[54.73%] pt-10 dark:from-[rgba(53,55,64,0)] dark:to-[#353740] dark:to-[58.85%]">
             <div className="w-full max-w-4xl mx-auto flex flex-col items-center px-4 space-y-4">
-                <Button
-                    icon={MdRefresh}
-                    className="font-medium"
-                    variant="primary"
-                >
-                    重新生成
-                </Button>
+                {messageList.length !== 0 && (
+                    streamingId !== '' ?
+                        (<Button
+                            icon={PiStopBold}
+                            className="font-medium"
+                            variant="primary"
+                        >
+                            停止生成
+                        </Button>)
+                        :
+                        (<Button
+                            icon={MdRefresh}
+                            className="font-medium"
+                            variant="primary"
+                        >
+                            重新生成
+                        </Button>)
+                )}
                 <div className="flex items-end w-full border border-black/10 dark:border-gray-800/50 bg-white dark:bg-gray-700 rounded-lg shadow-[0_0_15px_rgba(0,0,0,0.1)] py-4">
                     <div className="mx-3 mb-2.5">
                         <PiLightningFill />
