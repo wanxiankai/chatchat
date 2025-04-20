26128eac608d9b88c1223342de8aa00a444688b5
wanxiankai <xiankai.wan@gmail.com>
2025-04-21 01:40:28 +0800

    feat: remove the export prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,7 +3,7 @@ import { auth } from '@clerk/nextjs/server'
 
 // 创建Prisma实例
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+const prisma = globalForPrisma.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
