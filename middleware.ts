b8eb730b2cb1dccf759d723f5cadc97b8349d831
wanxiankai <xiankai.wan@gmail.com>
2025-04-21 22:53:17 +0800

    feat: reset the middleware logic

--- middleware.ts
+++ middleware.ts
@@ -3,9 +3,9 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
-  // if (!isPublicRoute(req)) {
-  //   await auth.protect()
-  // }
+  if (!isPublicRoute(req)) {
+    await auth.protect()
+  }
   console.log('Middleware triggered')
 })
 
